ecf3d8d531a1ff3b1065ed3875ee12cc157ec5d2
Joel Doerfel <71906733+joel-ruggable@users.noreply.github.com>
2022-02-10 14:31:53 -0800

    Create discover.py

---
+++ tap_facebook/discover.py
@@ -0,0 +1,14 @@
+import singer
+from tap_facebook.streams import STREAMS
+
+def discover_streams(client):
+    streams = []
+
+    for stream in STREAMS.values():
+        stream = stream(client)
+        schema = singer.resolve_schema_references(stream.load_schema())
+        streams.append({'stream': stream.name,
+                        'tap_stream_id': stream.name,
+                        'schema': schema,
+                        'metadata': stream.load_metadata()})
+    return streams
